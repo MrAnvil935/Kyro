108f497fb658a4dfaa730fea385e3ad7b4800e3c
MrAnvil935 <71383587+MrAnvil935@users.noreply.github.com>
2020-09-16 21:33:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ bot.on('messageCreate', (msg) => {
 }, 500);
   }
   
-  	 if(msg.content.includes('fortnite') || msg.content.includes('Fortnite') || msg.content.includes('Minecraft') || msg.content.includes('minecraft') || msg.content.includes('Terraria') || msg.content.includes('terraria') || msg.content.includes('LoL') || msg.content.includes('league of legends') || msg.content.includes('League of Legends')){                 
+  	 if(msg.content.includes('fortnite') || msg.content.includes('Fortnite') || msg.content.includes('Minecraft') || msg.content.includes('minecraft') || msg.content.includes('Terraria') || msg.content.includes('terraria') || msg.content.includes('LoL') || msg.content.includes('league of legends') || msg.content.includes('League of Legends') || msg.content.includes('League of legends')){                 
        setTimeout(function(){ 
     bot.createMessage(msg.channel.id, 'brawlhalla better');  
 }, 500);
@@ -90,7 +90,7 @@ bot.on('messageCreate', (msg) => {
 }, 500);
   }
 	
-	if(msg.content.includes('shadow legends') || msg.content.includes('Shadow Legends')) {                 
+	if(msg.content.includes('shadow legends') || msg.content.includes('Shadow Legends') || msg.content.includes('RAID')) {                 
        setTimeout(function(){ 
     bot.createMessage(msg.channel.id, 'RAID: Shadow Legends™ is an immersive online experience with everything you\'d expect from a brand new RPG title. It\'s got an amazing storyline, awesome 3D graphics, giant boss fights, PVP battles, and hundreds of never before seen champions to collect and customize.I never expected to get this level of performance out of a mobile game. Look how crazy the level of detail is on these champions!RAID: Shadow Legends™ is getting big real fast, so you should definitely get in early. Starting now will give you a huge head start. There\'s also an upcoming Special Launch Tournament with crazy prizes! And not to mention, this game is absolutely free!So go ahead and check out the video description to find out more about RAID: Shadow Legends™. There, you will find a link to the store page and a special code to unlock all sorts of goodies. Using the special code, you can get 50,000 Silver immediately, and a FREE Epic Level Champion as part of the new players program, courtesy of course of the RAID: Shadow Legends™ devs.');  
 }, 500);
