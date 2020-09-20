71a31730393cb1cfc9e222cec26b0be45c1b5934
MrAnvil935 <71383587+MrAnvil935@users.noreply.github.com>
2020-09-21 01:54:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ bot.on("ready", () => { // When the bot is ready
  
  
 bot.on('messageCreate', (msg) => { 
-if (message.member.roles.find(r => r.name === "NoKyro") || msg.channel.id != '756401543397245018')   {} 
+if (message.member.roles.has('756401543397245018'.id) || msg.channel.id != '756401543397245018')   {} 
 else
   {            
     
