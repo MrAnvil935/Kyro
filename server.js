a82576bbefe18da193548f5cb20c8bc88771d7bf
MrAnvil935 <71383587+MrAnvil935@users.noreply.github.com>
2020-09-21 01:29:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ bot.on("ready", () => { // When the bot is ready
  
  
 bot.on('messageCreate', (msg) => { 
-if (role.id != '754416269423411251' ||  msg.channel.id === '756401543397245018')     {            
+if (msg.role.id != '754416269423411251' ||  msg.channel.id === '756401543397245018')     {            
     
 	if (!msg.author.bot)
 	{
