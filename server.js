07fdb711d1d3c830df51d49b19c1e898980fc66a
MrAnvil935 <71383587+MrAnvil935@users.noreply.github.com>
2020-09-23 20:50:39 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bot = new Eris(process.env.token);
 // Replace BOT_TOKEN with your bot account's token
  
 bot.on("ready", () => { // When the bot is ready
-bot.user.game.name === "Brawlhalla";   
+client.user.setStatus('dnd', 'Made by KwinkyWolf') 
 console.log("Ready!"); // Log "Ready!"
 });
  
