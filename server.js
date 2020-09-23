e0244dc167d35f7a38a0fd37af302f40c18c4e2c
MrAnvil935 <71383587+MrAnvil935@users.noreply.github.com>
2020-09-23 23:57:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,286 @@
 const Eris = require("eris");
  
-var client = new Eris(process.env.token);
+var bot = new Eris(process.env.token);
 // Replace BOT_TOKEN with your bot account's token
+
+ var status = "dnd"
+var game = new Object();
+game.name = 'Brawlhalla';
  
-client.on("ready", () => { // When the bot is ready
+bot.on("ready", () => { // When the bot is ready
+bot.editStatus(status, game);
 console.log("Ready!"); // Log "Ready!"
 });
  
 
  
-client.on('messageCreate', (msg) => {                     
+bot.on('messageCreate', (msg) => {                     
     
 	
 	if (!msg.author.bot)
 	{
 		
 		
+		
+	if(msg.content.includes('yah') && msg.content.includes('fortnite')&& msg.content.includes('angeli') && msg.content.includes('animal'))
+         { bot.createMessage(msg.channel.id, 'fuck off'); }	
+		
+		else 
+		{	
+	var y =(Math.floor((Math.random() * 1000) + 1))	
+	if (y==97 || y==98 || y==99 || y==100)
+	   {bot.createMessage(msg.channel.id, 'I got drag pet');  }	
+		
+	if(msg.content.includes('dog') || msg.content.includes('DOG') || msg.content.includes('Dog')  ) {                 
+       setTimeout(function(){ 
+    bot.createMessage(msg.channel.id, 'cat > dog');   
+}, 500);
+  }
+  
+    else if (msg.channel.id != '677876231609253914') {
+    if((msg.content === 't!cat')) {                 
+       setTimeout(function(){ 
+     if (x==69)
+	   {bot.createMessage(msg.channel.id, 'CAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAT');  }
+       else {
+	   if (x>75) {
+	   bot.createMessage(msg.channel.id, 'CAAAT');  }
+       else { bot.createMessage(msg.channel.id, 'CAT');}}
+}, 2500);
+  }}
+		
+	if ((msg.content != 't!cat')) { 	
+	if(msg.content.includes('c a t') || msg.content.includes('Cat')|| msg.content.includes('CAT') || msg.content.includes('cat')) {  
+   if (msg.channel.id != '677876231609253914') {	
+   var x =(Math.floor((Math.random() * 100) + 1))		
+       setTimeout(function(){ 
+	   if (x==69)
+	   {bot.createMessage(msg.channel.id, 'CAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAT');  }
+       else {
+	   if (x>75) {
+	   bot.createMessage(msg.channel.id, 'CAAAT');  }
+       else { bot.createMessage(msg.channel.id, 'CAT');}}
+}, 500);
+	}}}
+		
+	if(msg.content.includes('got scammed')) {                 
+       setTimeout(function(){ 
+    bot.createMessage(msg.channel.id, 'L');  
+}, 500);
+  }
   
   if (msg.channel.id != '677876231609253914') {
     if(msg.content.includes('YAH') || msg.content.includes('yah') || msg.content.includes('Yah')) {                 
        setTimeout(function(){ 
-    client.createMessage(msg.channel.id, 'YAH');  
+    bot.createMessage(msg.channel.id, 'YAH');  
 }, 500);
   }}
 	
 	
-		
+	
+	 if(msg.content.includes('@everyone') || msg.content.includes('@755098192038854686') || msg.content.includes('@!755098192038854686')) {                 
+       setTimeout(function(){ 
+    bot.createMessage(msg.channel.id, 'bru');  
+}, 500);
+  }
+  
+
+  if (msg.channel.id != '677876231609253914') {
+  	 if(msg.content.includes('fortnite') || msg.content.includes('Fortnite') || msg.content.includes('Minecraft') || msg.content.includes('minecraft') || msg.content.includes('Terraria') || msg.content.includes('terraria') || msg.content.includes('LoL') || msg.content.includes('league of legends') || msg.content.includes('League of Legends') || msg.content.includes('League of legends')){                 
+    
+	   setTimeout(function(){ 
+    bot.createMessage(msg.channel.id, 'brawlhalla better');  
+}, 500);
+  }}
+	
+	  if(msg.content.includes('m poor')) {                 
+       setTimeout(function(){ 
+    bot.createMessage(msg.channel.id, 'just drop drag pet ez');  
+}, 500);
+  }
+  
+  if (msg.channel.id != '677876231609253914') {
+	  var x =(Math.floor((Math.random() * 10) + 1))
+	  if (x<4) {
+  	  if(msg.content.includes('animal') || msg.content.includes('Animal')) {                 
+       setTimeout(function(){ 
+    bot.createMessage(msg.channel.id, 'cats>everything');  
+}, 500);
+  }}}
+  
+  if (msg.channel.id != '677876231609253914') {
+    if(msg.content.includes('BRAZIL') || msg.content.includes('brazil') || msg.content.includes('Brazil')) {                 
+       setTimeout(function(){ 
+    bot.createMessage(msg.channel.id, 'BRAZIL');  
+}, 500);
+  }}
+	
+	if(msg.content.includes('Angeli') || msg.content.includes('angeli') || msg.content.includes('ANGELI')) {                 
+       setTimeout(function(){ 
+    bot.createMessage(msg.channel.id, 'ANGELI is DEAD');  
+}, 500);
+  }
+  
+  if (msg.channel.id != '677876231609253914') {
+	  var x =(Math.floor((Math.random() * 10) + 1))
+	  if (x<3) {
+  	if(msg.content.includes('god') || msg.content.includes('God') || msg.content.includes('GOD')) {                 
+       setTimeout(function(){ 
+    bot.createMessage(msg.channel.id, 'I\'m god of cats');  
+}, 500);
+  }}}
+	
+	if(msg.content.includes('fragile ego')) {                 
+       setTimeout(function(){ 
+    bot.createMessage(msg.channel.id, 'look whos talking');  
+}, 500);
+  }
+	
+   if (msg.channel.id != '677876231609253914') {	
+	if(msg.content.includes('shadow legends') || msg.content.includes('Shadow Legends') || msg.content.includes('RAID')) {                 
+       setTimeout(function(){ 
+    bot.createMessage(msg.channel.id, 'RAID: Shadow Legends™ is an immersive online experience with everything you\'d expect from a brand new RPG title. It\'s got an amazing storyline, awesome 3D graphics, giant boss fights, PVP battles, and hundreds of never before seen champions to collect and customize.I never expected to get this level of performance out of a mobile game. Look how crazy the level of detail is on these champions!RAID: Shadow Legends™ is getting big real fast, so you should definitely get in early. Starting now will give you a huge head start. There\'s also an upcoming Special Launch Tournament with crazy prizes! And not to mention, this game is absolutely free!So go ahead and check out the video description to find out more about RAID: Shadow Legends™. There, you will find a link to the store page and a special code to unlock all sorts of goodies. Using the special code, you can get 50,000 Silver immediately, and a FREE Epic Level Champion as part of the new players program, courtesy of course of the RAID: Shadow Legends™ devs.');  
+}, 500);
+  }} 
+  
+  	 if(msg.content.includes('italy') || msg.content.includes('Italy')) {                 
+       setTimeout(function(){ 
+    bot.createMessage(msg.channel.id, 'i dont speak mafia');  
+}, 500);
+  }
+	
+	
+	if (msg.channel.id != '677876231609253914') {
+	if(msg.content.includes('TNTnearchos')) {                 
+       setTimeout(function(){ 
+    bot.createMessage(msg.channel.id, 'haha autoclickr');  
+}, 500);
+	}}
+  
+  if (msg.channel.id != '677876231609253914') {
+	  var x =(Math.floor((Math.random() * 10) + 1))
+	  if (x<6) {
+  	if(msg.content.includes('boomer') || msg.content.includes('Boomer') || msg.content.includes('BOOMER')) {                 
+       setTimeout(function(){ 
+    bot.createMessage(msg.channel.id, 'ok boomer');  
+}, 500);
+  }}}
+  
+  if (msg.channel.id != '677876231609253914') {
+    if(msg.content.includes('vnj') || msg.content.includes('Vnj') || msg.content.includes('VNJ')) {                 
+       setTimeout(function(){ 
+    bot.createMessage(msg.channel.id, ':clown:');  
+}, 500);
+  }}
+  
+  if (msg.channel.id != '677876231609253914') {
+	  var y =(Math.floor((Math.random() * 10) + 1))
+	  if (y<6) {
+    if(msg.content.includes('pog') || msg.content.includes('Pog') || msg.content.includes('POG')) {     
+ var x =(Math.floor((Math.random() * 100) + 1))	
+       setTimeout(function(){ 
+       if (x==69)
+	   {bot.createMessage(msg.channel.id, 'POOOOOOOOOOOG');  }
+       else {
+	   if (x>75) {
+	   bot.createMessage(msg.channel.id, 'POG');  }
+       else { bot.createMessage(msg.channel.id, 'pog');}}
+}, 500);
+  }}}
+	
+	if (msg.channel.id != '677876231609253914') {
+	if(msg.content === 'k!help') {                 
+       var x =(Math.floor((Math.random() * 100) + 1))
+	   setTimeout(function(){ 
+	   if (x==69)
+	   {bot.createMessage(msg.channel.id, 'Key words: cat, dog, got scammed, yah, @ ping, fortnite, I\'m poor, animal, Brazil, angeli, god, fragile ego, RAID Shadow Legends, Italy, OwO , TNTnearchos, boomer, vnj, pog, wanna do drag, suck, superior, donate to me, what drag');  }
+       else {
+	   bot.createMessage(msg.channel.id, 'no');  }
+}, 500);
+	}}
+  
+  
+  	if(msg.content.includes('wanna do drag')) {                 
+       setTimeout(function(){ 
+    bot.createMessage(msg.channel.id, 'lemme start my macro');  
+}, 500);
+  }
+  
+  if (msg.channel.id != '677876231609253914') {
+	  var x =(Math.floor((Math.random() * 10) + 1))
+	  if (x<6) {
+    if(msg.content.includes('suck') || msg.content.includes('SUCK')) {                 
+       setTimeout(function(){ 
+    bot.createMessage(msg.channel.id, 'no u');  
+}, 500);
+  }}}
+  
+      if(msg.content.includes('Superior') || msg.content.includes('superior') || msg.content.includes('SUPERIOR')) {                 
+       var x =(Math.floor((Math.random() * 10) + 1))
+	  if (x<4) {
+	   if (msg.channel.id != '677876231609253914') {	
+	   setTimeout(function(){ 
+    bot.createMessage(msg.channel.id, 'superior? warp meee');  
+}, 500);
+	  }}}
+  
+     if(msg.content.includes('donate to me') || msg.content.includes('Donate to me')) {                 
+       setTimeout(function(){ 
+    bot.createMessage(msg.channel.id, 'https://vignette.wikia.nocookie.net/hypixel-skyblock/images/9/99/Ender_Armor.png');  
+}, 500);
+  }
+  
+  if (msg.channel.id != '677876231609253914') {
+      if(msg.content.includes('what drag') || msg.content.includes('What drag')) {     
+ var x =(Math.floor((Math.random() * 100) + 1))	
+       setTimeout(function(){ 
+       if (x==1 || x==2 || x==3 || x==4 || x==5 || x==6 || x==7 || x==8 || x==9 || x==10 || x==11 || x==12 || x==13 || x==14 || x==15 || x==16)
+	   {bot.createMessage(msg.channel.id, 'protector');  }
+       if (x==17 || x==18 || x==19 || x==20 || x==21 || x==22 || x==23 || x==24 || x==25 || x==26 || x==27 || x==28 || x==28 || x==30 || x==31 || x==32)
+	   {bot.createMessage(msg.channel.id, 'old');  }
+       if (x==33 || x==34 || x==35 || x==36 || x==37 || x==38 || x==39 || x==40 || x==41 || x==42 || x==43 || x==44 || x==45 || x==46 || x==47 || x==48)
+	   {bot.createMessage(msg.channel.id, 'unstable');  }
+       if (x==49 || x==50 || x==51 || x==52 || x==53 || x==54 || x==55 || x==56 || x==57 || x==58 || x==59 || x==60 || x==61 || x==62 || x==63 || x==64)
+	   {bot.createMessage(msg.channel.id, 'wise');  }
+       if (x==65 || x==66 || x==67 || x==68 || x==69 || x==70 || x==71 || x==72 || x==73 || x==74 || x==75 || x==76 || x==77 || x==78 || x==79 || x==80)
+	   {bot.createMessage(msg.channel.id, 'young');  }
+       if (x==81 || x==82 || x==83 || x==84 || x==85 || x==86 || x==87 || x==88 || x==89 || x==90 || x==91 || x==92 || x==93 || x==94 || x==95 || x==96)
+	   {bot.createMessage(msg.channel.id, 'strong');  }
+	   if (x==97 || x==98 || x==99 || x==100)
+	   {bot.createMessage(msg.channel.id, 'superior :D');  }
+}, 500);
+  }}
+  
+  if (msg.channel.id != '677876231609253914') {
+if(msg.content.includes('owo') || msg.content.includes('OWO') || msg.content.includes('OwO') || msg.content.includes('uwu') || msg.content.includes('UWU') || msg.content.includes('UwU'))       {         
+       var x =(Math.floor((Math.random() * 10) + 1))
+	   setTimeout(function(){ 
+	   if (x==1)
+	   {bot.createMessage(msg.channel.id, '```=^._.^= ∫```');  }
+       if (x==2)
+	   {bot.createMessage(msg.channel.id, '```(=^･ω･^=)```');  }
+       if (x==3)
+	   {bot.createMessage(msg.channel.id, '```/ᐠ｡ꞈ｡ᐟ\\ ```');  }
+       if (x==4)
+	   {bot.createMessage(msg.channel.id, '```[^._.^]ﾉ彡```');  }
+	   if (x==5)
+	   {bot.createMessage(msg.channel.id, '```—ฅ/ᐠ. ̫ .ᐟ\\ฅ —```');  }
+       if (x==6)
+	   {bot.createMessage(msg.channel.id, '```/ᐠ. ｡.ᐟ\\ᵐᵉᵒʷ```');  }
+       if (x==7)
+	   {bot.createMessage(msg.channel.id, '```/ᐠﹷ ‸ ﹷ ᐟ\\ﾉ```');  }
+       if (x==8)
+	   {bot.createMessage(msg.channel.id, '```(ﾐΦ ﻌ Φﾐ)∫```');  }
+       if (x==9)
+	   {bot.createMessage(msg.channel.id, '```/ᐠ=ᆽ=ᐟ \\ ```');  }
+       if (x==10)
+	   {bot.createMessage(msg.channel.id, '```ᨐᵉᵒʷ ```');  }
+   
+}, 500);
+  }}
+	
+		}}
  });
 
  
-client.connect(); 
+bot.connect(); 
