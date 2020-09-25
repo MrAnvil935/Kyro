cc08021259649811f1b761e0ddcddb83c3258f55
MrAnvil935 <71383587+MrAnvil935@users.noreply.github.com>
2020-09-25 19:44:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ bot.on('messageCreate', (msg) => {
 		
 	if ((msg.content != 't!cat')) { 	
 	if(msg.content.includes('c a t') || msg.content.includes('Cat')|| msg.content.includes('CAT') || msg.content.includes('cat')) {  
-   if(msg.content.includes('catac') {}
+   if(msg.content.includes('catac')) {}
    else {
    if (msg.channel.id != '677876231609253914') {	
    var x =(Math.floor((Math.random() * 100) + 1))		
