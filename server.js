9f9a314bd9976286b4076ac47c38273c2bf85d58
MrAnvil935 <71383587+MrAnvil935@users.noreply.github.com>
2020-12-14 00:31:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -147,6 +147,12 @@ if (msg.author.id == '521672404242792448')	{
 }, 500);		
 }
 
+if (msg.author.id == '303234870451240960')	{	
+ setTimeout(function(){ 
+    bot.addMessageReaction(msg.channel.id, msg.id, "hoe_thonk:754351845396185208")
+}, 500);		
+}
+
 if (msg.author.id == '394489126310641664')	{	
  var w =(Math.floor((Math.random() * 5) + 1))
 	if (w==3){
