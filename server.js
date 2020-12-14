ee8446d0372d6d73a4c876bb6e827b1ee821e740
MrAnvil935 <71383587+MrAnvil935@users.noreply.github.com>
2020-12-14 16:19:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -147,7 +147,7 @@ if (msg.author.id == '521672404242792448')	{
 }, 500);		
 }
 
-if (msg.author.id == '470582170151550977')	{	
+if (msg.author.id == '771734443559878666')	{	
  setTimeout(function(){ 
     bot.addMessageReaction(msg.channel.id, msg.id, "holdthis:766889368228200448")
 }, 500);		
