dfe016fb55295ef7538eeb53e93e273edf247ed0
MrAnvil935 <71383587+MrAnvil935@users.noreply.github.com>
2021-03-16 15:30:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -255,7 +255,7 @@ if (msg.author.id == '670769970396790795')	{
 	if(msg.content.toLowerCase().includes('nefersit')) {                 
        setTimeout(function(){ 
     bot.createMessage(msg.channel.id, '!ban <@563397766324420608>');  
-}, 500);
+}, 300);
   }
 	
    if (msg.channel.id != '677876231609253914') {	
@@ -331,7 +331,7 @@ if (msg.author.id == '670769970396790795')	{
        var k =(Math.floor((Math.random() * 100) + 1))
 	   setTimeout(function(){ 
 	   if (k==69)
-	   {bot.createMessage(msg.channel.id, 'Key words: cat, dog, got scammed, bruh, yah, @ ping, macro, egirl, toyota,fortnite, I\'m poor, animal, Brazil, angeli, anime bad, god, fragile ego, RAID Shadow Legends, Brawlhalla, Italy, OwO , TNTnearchos, boomer, vnj, ME, YOU, have friend, pog, wanna do drag, suck, superior, stfu Kyro, donate to me, what drag');  }
+	   {bot.createMessage(msg.channel.id, 'Key words: cat, dog, got scammed, bruh, yah, @ ping, macro, egirl, toyota, nefersit, fortnite, I\'m poor, animal, Brazil, angeli, anime bad, god, fragile ego, RAID Shadow Legends, Brawlhalla, Italy, OwO , TNTnearchos, boomer, vnj, ME, YOU, have friend, pog, wanna do drag, suck, superior, stfu Kyro, donate to me, what drag');  }
        else {
 	   bot.createMessage(msg.channel.id, 'no');  }
 }, 500);
