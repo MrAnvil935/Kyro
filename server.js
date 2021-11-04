81aab524b5a5a61de09950eb32793ee0ae186d96
MrAnvil935 <71383587+MrAnvil935@users.noreply.github.com>
2021-11-04 19:19:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -408,6 +408,12 @@ if (msg.author.id == '670769970396790795')	{
 }, 500);
   }
 
+ 	 if(msg.content.toLowerCase().includes('ping tnt')) {                 
+       setTimeout(function(){ 
+    bot.createMessage(msg.channel.id, '<@221381845739110402>');  
+}, 500);
+  }
+
      if(msg.content.toLowerCase().includes('macro')) {                 
        setTimeout(function(){ 
     bot.createMessage(msg.channel.id, 'https://cdn.discordapp.com/attachments/756401543397245018/808795891192299550/angeli.png');  
