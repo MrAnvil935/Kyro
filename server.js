6dac2b6a575e289ee00b3492cd013c7bf1cb37d5
MrAnvil935 <71383587+MrAnvil935@users.noreply.github.com>
2021-11-16 17:28:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -364,7 +364,7 @@ if (msg.author.id == '670769970396790795')	{
        var k =(Math.floor((Math.random() * 50) + 1))
 	   setTimeout(function(){ 
 	   if (k==33)
-	   {bot.createMessage(msg.channel.id, 'Key words: cat, dog, got scammed, bruh, yah, what drop, @ ping, macro, egirl, toyota, xd, rgb, ping tnt, genshin, no, nefersit, gay, fortnite, fuck off tori, cock, puta, I\'m poor, animal, Brazil, angeli, ping tori, anime bad, god, fragile ego, RAID Shadow Legends, Brawlhalla, Italy, OwO , TNTnearchos, boomer, vnj, ME, YOU, have friend, pog, wanna do drag, suck, superior, gojo, stfu Kyro, donate to me, what drag');  }
+	   {bot.createMessage(msg.channel.id, 'Key words: cat, dog, got scammed, bruh, yah, what drop, @ ping, macro, egirl, toyota, xd, rgb, ping tnt, genshin, no, nefersit, gay, fortnite, fuck off tori, cock, puta, I\'m poor, animal, Brazil, angeli, ping tori, anime bad, padoru, god, fragile ego, RAID Shadow Legends, Brawlhalla, Italy, OwO , TNTnearchos, boomer, vnj, ME, YOU, have friend, pog, wanna do drag, suck, superior, gojo, stfu Kyro, donate to me, what drag');  }
        else {
 	   bot.createMessage(msg.channel.id, 'no');  }
 }, 500);
@@ -434,6 +434,12 @@ if(msg.content.toLowerCase().includes('ping tori')) {
      if(msg.content.toLowerCase().includes('macro')) {                 
        setTimeout(function(){ 
     bot.createMessage(msg.channel.id, 'https://cdn.discordapp.com/attachments/756401543397245018/808795891192299550/angeli.png');  
+}, 500);
+  }
+
+if(msg.content.toLowerCase().includes('padoru')) {                 
+       setTimeout(function(){ 
+    bot.createMessage(msg.channel.id, 'https://media.discordapp.net/attachments/692723580118499349/909106815206309888/padoru.gif');  
 }, 500);
   }
 	
