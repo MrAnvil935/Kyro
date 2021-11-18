5f47c1c178af1ea619e9847ccdadd6ca34d23d4d
MrAnvil935 <71383587+MrAnvil935@users.noreply.github.com>
2021-11-18 12:41:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bot = new Eris(process.env.token);
 var status = "dnd"
 var game = new Object();
 game.name = 'Super Hentai'
-game.type = 2
+
 game.url = `https://youtu.be/dQw4w9WgXcQ`
  
 bot.on("ready", () => {
