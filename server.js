a294c6e99cbd834e69fb1cdfbe11b8174725f5e5
MrAnvil935 <71383587+MrAnvil935@users.noreply.github.com>
2022-08-25 15:17:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -265,7 +265,7 @@ if (msg.author.id == '670769970396790795')	{
 if(msg.content.startsWith('Kyro say')) {                 
        setTimeout(function(){ 
    var ks = msg.content
-   ks = ks.replace(" Kyro say", "")
+   ks = ks.replace("Kyro say", "")
    bot.createMessage.(msg.channel.id, ks+"_ _");  
 }, 500);
   }	
