041e575b7b91cf74c481bfd52c63771f86d6ec83
MrAnvil935 <71383587+MrAnvil935@users.noreply.github.com>
2022-08-29 10:14:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -203,10 +203,12 @@ bot.createMessage(msg.channel.id, troll+" xd");
 if(msg.content.toLowerCase().includes('kyro long cat')) {            
        setTimeout(function(){ 
 var longcat = (Math.floor((Math.random() * 25) + 1))
-	
-         bot.createMessage(msg.channel.id, "O" + '='.repeat(longcat) + "-" );  
+
+         bot.createMessage(msg.channel.id, ':cat:'.repeat(longcat) );  
 }, 500);
   }
+
+
 		
 if(msg.content.toLowerCase().includes('egrill') || msg.content.toLowerCase().includes('egirl')) {                 
        setTimeout(function(){ 
