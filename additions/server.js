e0fd937aa3c7bf28f35cbea9657144fcdabf2507
MrAnvil935 <71383587+MrAnvil935@users.noreply.github.com>
2022-11-16 22:08:25 +0100

    Add files via upload

---
+++ additions/server.js
@@ -0,0 +1,30 @@
+/*
+	this bot is a ping pong bot, and every time a message
+	beginning with "ping" is sent, it will reply with
+	"pong!".
+*/
+
+// Load discord.js
+var Discord = require("discord.js");
+
+// Create a client
+var bot = new Discord.Client();
+
+// This code will run once the bot has started up.
+bot.on("ready", function () {
+	bot.user.setPresence({
+    status: 'dnd',
+})
+	console.log("Ready to begin! Serving in " + bot.channels.length + " channels");
+});
+
+
+// This code will run once the bot receives any message.
+bot.on('message', message => {
+  if (message.content === 'kping') {  
+    message.channel.send(`Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ws.ping)}ms`);
+  }
+});
+
+// Login (replace these auth details with your bot's)
+bot.login(process.env.token);
