d55ae0a539091c2c1f3e5fb092b9693f32dc9905
ArtificialAngel <71383587+MrAnvil935@users.noreply.github.com>
2022-11-25 16:31:48 +0100

    Update server.js

--- additions/server.js
+++ additions/server.js
@@ -39,7 +39,7 @@ client.on("ready", async () => {
   	client.user.setPresence({
     status: 'dnd',
 })
-  client.application.commands.set([]);
+  client.applicationCommands.set([]);
 });
 
 // declaring cache db to store game data
